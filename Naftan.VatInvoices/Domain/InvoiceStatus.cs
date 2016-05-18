9fdcb76ab30b565f6714573dd72bb0c760541ce4
Dmitry <dmtKlenye@gmail.com>
2016-05-18 09:31:06 +0300

    После обращения в службу поддержки доработаны статусы ЭСЧФ

--- Naftan.VatInvoices/Domain/InvoiceStatus.cs
+++ Naftan.VatInvoices/Domain/InvoiceStatus.cs
@@ -7,12 +7,12 @@ namespace Naftan.VatInvoices.Domain
     /// </summary>
     public enum InvoiceStatus
     {
-        [Description("В разработке")] A = 1,
-        [Description("В разработке. Ошибка")] B = 2,
+        [Description("В разработке")] IN_PROGRESS = 1,
+        [Description("В разработке. Ошибка")] IN_PROGRESS_ERROR = 2,
         [Description("Выставлен")] COMPLETED = 3,
         [Description("Выставлен. Подписан получателем")] COMPLETED_SIGNED = 4,
-        [Description("Выставлен. Начато аннулирование")] E = 5,
-        [Description("На согласовании")] F = 6,
-        [Description("Аннулирован")] G = 7,
+        [Description("Выставлен. Аннулирован поставщиком")] ON_AGREEMENT_CANCEL = 5,
+        [Description("На согласовании")] ON_AGREEMENT = 6,
+        [Description("Аннулирован")] CANCELLED = 7
     }
 }
