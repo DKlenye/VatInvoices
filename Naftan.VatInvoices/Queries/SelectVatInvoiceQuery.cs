3b646fc96165e58c94dabcbb51ef7b66ca3e68fa
Dmitry <dmtKlenye@gmail.com>
2016-05-18 09:35:39 +0300

    Добавлен запрос на получение полной версии данных по ЭСЧФ для серилизации

---
+++ Naftan.VatInvoices/Queries/SelectVatInvoiceQuery.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using System.Linq;
+using Dapper;
+using Naftan.VatInvoices.Domain;
+using Naftan.VatInvoices.Extensions;
+using Naftan.VatInvoices.QueryObjects;
+
+namespace Naftan.VatInvoices.Queries
+{
+    public class SelectVatInvoiceQuery:AbstractQuery<VatInvoice>
+    {
+
+        public int InvoiceId { get; private set; }
+
+        public SelectVatInvoiceQuery(IDbConnection connection, int invoiceId) : base(connection)
+        {
+            InvoiceId = invoiceId;
+        }
+
+        public override VatInvoice Query()
+        {
+            var transaction = Connection.BeginTransaction();
+
+            var vatInvoice = Connection.Query<VatInvoiceNumber,VatInvoice,VatInvoice>(new SelectVatInvoice().ById(InvoiceId).Sql,
+                (n, i) =>
+                {
+                    i.VatNumber = n;
+                    return i;
+                }, transaction:transaction,splitOn:"InvoiceId").Single();
+            
+            vatInvoice.Documents = Connection.Query<Document>(new SelectDocument().ByInvoiceId(InvoiceId),transaction);
+         
+            vatInvoice.Consignees = Connection.Query<Consignee>(new SelectConsignee().ByInvoiceId(InvoiceId),transaction);
+            
+            vatInvoice.Consignors = Connection.Query<Consignor>(new SelectConsignor().ByInvoiceId(InvoiceId),transaction);
+            
+            vatInvoice.RosterList = Connection.Query<Roster>(new SelectRoster().ByInvoiceId(InvoiceId),transaction);
+
+            transaction.Commit();
+
+            return vatInvoice;
+        }
+    }
+}
