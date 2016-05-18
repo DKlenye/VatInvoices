97eaf9d02cb84d3ff2f5959b391d3edbdbe550ab
Dmitry <dmtKlenye@gmail.com>
2016-05-18 09:27:08 +0300

    Добалены классы для работы с пользователями

---
+++ Naftan.VatInvoices/Users/CurrentUser.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Security.Principal;
+
+namespace Naftan.VatInvoices.Users
+{
+    public static class CurrentUser
+    {
+        private const char DomainSplitter = '\\';
+
+        private static WindowsPrincipal GetPrincipal()
+        {
+            var identity = WindowsIdentity.GetCurrent() ?? WindowsIdentity.GetAnonymous();
+            return new WindowsPrincipal(identity);
+        }
+
+        private static string IdentityName
+        {
+            get { return GetPrincipal().Identity.Name; }
+        }
+
+        public static string Login
+        {
+            get { return IdentityName.Split(DomainSplitter)[1]; }
+        }
+
+        public static string Name
+        {
+            get
+            {
+                var context = UserPrincipal.Current;
+                return context.DisplayName;
+            }
+        }
+
+        public static IEnumerable<string> Roles
+        {
+            get
+            {
+                var userRoles = new List<string>();
+                var principal = GetPrincipal();
+
+                Enum.GetNames(typeof (UserRoles)).ToList().ForEach(x =>
+                {
+                    if (principal.IsInRole(x))
+                    {
+                        userRoles.Add(x);
+                    }
+                });
+
+                return userRoles;
+            }
+        }
+
+        private static bool IsInRole(UserRoles role)
+        {
+            return GetPrincipal().IsInRole(role.ToString());
+        }
+
+        public static bool IsAdmin()
+        {
+            return IsInRole(UserRoles.NDSInvoices_Admins);
+        }
+
+    }
+}
---
+++ Naftan.VatInvoices/Users/UserRoles.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Naftan.VatInvoices.Users
+{
+    /// <summary>
+    /// Роли пользователей
+    /// </summary>
+    public enum UserRoles
+    {
+        [Description("Администратор")] NDSInvoices_Admins,
+        [Description("Пользователь")] NDSInvoices_Users
+    }
+}
